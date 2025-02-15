c13a8c63750c000601fc8227179fe36979f6a034
Sankalp <140296557+quantumscribe@users.noreply.github.com>
2025-02-15 11:43:57 +0530

    Refactored Lec-3 code

--- Lec-3/recursion.c
+++ Lec-3/recursion.c
@@ -1,35 +1,40 @@
-// same patter as pattern.c but using recursion
+// Program to draw a pyramid pattern using recursion
 
 #include <stdio.h>
 
+// Function prototype
 void draw(int n);
 
 int main()
 {
 	int n;
+	// Prompt user for the number of rows
 	printf("Enter the number of rows: ");
 	scanf("%d", &n);
+	// Call the draw function
 	draw(n);
 	return 0;
 }
 
+// Recursive function to draw the pyramid
 void draw(int n)
 {
-	// if nothing to draw (basecase)
+	// Base case: if nothing to draw
 	if (n == 0)
 	{
 		return;
 	}
 	else
 	{
-		//print pyramid of height n-1
+		// Recursive call to draw pyramid of height n-1
 		draw(n - 1);
 
-		//print one more row
+		// Print one more row of the pyramid
 		for (int i = 0; i < n; i++)
 		{
 			printf("#");
 		}
+		// Move to the next line after printing the row
 		printf("\n");
 	}
 }
