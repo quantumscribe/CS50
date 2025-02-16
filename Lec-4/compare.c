c72a6080cf51b20438dbde78c263e6f3e55a26f4
Sankalp <140296557+quantumscribe@users.noreply.github.com>
2025-02-16 11:38:52 +0530

    Lec-4

--- Lec-4/compare.c
+++ Lec-4/compare.c
@@ -3,14 +3,21 @@
 
 int main(void)
 {
+	// Declare two character arrays to store the input strings
 	char s[20];
 	char t[20];
 
+	// Prompt the user to enter the first string and store it in array s
 	printf("Enter a string s: ");
 	scanf("%s", s);
+
+	// Prompt the user to enter the second string and store it in array t
 	printf("Enter a string t: ");
 	scanf("%s", t);
 
+	// Print the memory address of the first character of array s
 	printf("s : %p\n", s);
+
+	// Print the memory address of the first character of array t
 	printf("t : %p\n", t);
 }
--- Lec-4/copy.c
+++ Lec-4/copy.c
@@ -5,20 +5,30 @@
 
 int main(void)
 {
+	// Declare a character array to store the input string
 	char s[10];
 	printf("Enter a string: ");
+	// Read a string from the user
 	scanf("%s", s);
 
+	// Allocate memory for a new string that is the same length as the input string plus one for the null terminator
 	char *t = malloc(strlen(s) + 1);
+	// Copy each character from the input string to the new string
 	for (int i = 0, length = strlen(s); i <= length; i++)
 	{
 		t[i] = s[i];
 	}
+	// If the new string is not empty, convert the first character to uppercase
 	if (strlen(t) > 0)
 	{
 		t[0] = toupper(t[0]);
 	}
 
+	// Print the original string
 	printf("%s\n", s);
+	// Print the new string with the first character in uppercase
 	printf("%s\n", t);
+
+	// Free the allocated memory
+	free(t);
 }
